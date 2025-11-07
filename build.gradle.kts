d96834d4de510613444e17f3ee2b7cd6dd57f314
Artem.Bukhonov <artem.bukhonov@jetbrains.com>
2025-11-07 16:56:54 +0100

    Promote version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 private val buildNumber: String? = System.getenv("GITHUB_RUN_NUMBER")
 private val isReleasePublication = System.getenv("RELEASE_PUBLICATION")?.toBoolean() ?: false
 
-private val baseVersion = "0.4.0"
+private val baseVersion = "0.5.0"
 
 allprojects {
     group = "com.agentclientprotocol"
