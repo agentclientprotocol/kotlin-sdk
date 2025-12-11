cca1122803d8f8fb2aa910ea91d8b81c8758255e
Semyon Proshev <sproshev@users.noreply.github.com>
2025-12-11 20:32:10 +0100

    Make AgentInfo and ClientInfo serializable (#35)

--- acp/src/commonMain/kotlin/com/agentclientprotocol/client/ClientInfo.kt
+++ acp/src/commonMain/kotlin/com/agentclientprotocol/client/ClientInfo.kt
@@ -4,8 +4,10 @@ import com.agentclientprotocol.model.ClientCapabilities
 import com.agentclientprotocol.model.Implementation
 import com.agentclientprotocol.model.LATEST_PROTOCOL_VERSION
 import com.agentclientprotocol.model.ProtocolVersion
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonElement
 
+@Serializable
 public class ClientInfo(
     public val protocolVersion: ProtocolVersion = LATEST_PROTOCOL_VERSION,
     public val capabilities: ClientCapabilities = ClientCapabilities(),
