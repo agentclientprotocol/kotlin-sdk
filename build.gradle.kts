4a75f1db26340b778d85df8fbd89299bfc88d3ca
Anna.Zhdan <anna.zhdan@jetbrains.com>
2026-01-26 11:37:07 +0100

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 private val buildNumber: String? = System.getenv("GITHUB_RUN_NUMBER")
 private val isReleasePublication = System.getenv("RELEASE_PUBLICATION")?.toBoolean() ?: false
 
-private val baseVersion = "0.15.1"
+private val baseVersion = "0.15.2"
 
 allprojects {
     group = "com.agentclientprotocol"
